f305cb8b3e1fcbafa3f510007c96c76a9bb35f7a
Jake Wood <jzwood14@yahoo.com>
2025-10-28 10:39:43 -0400

    video works

--- web/main.js
+++ web/main.js
@@ -1,38 +1,56 @@
+function debug(text) {
+  const node = document.createTextNode(text);
+  const body = document.body;
+  body.appendChild(node);
+}
+
+const GRID = 15
+const PX_PER_SQUARE = 35
+const SIDE = PX_PER_SQUARE * GRID
+const SIDE_RGBA = 4 * SIDE
+
 async function setupPixelManipulation() {
   // Get camera access
-  const stream = await navigator.mediaDevices.getUserMedia({
-    video: { facingMode: "environment" },
-  });
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: "environment" },
+    });
+    const video = document.createElement("video");
+    video.srcObject = stream;
+    await video.play();
 
-  const video = document.createElement("video");
-  video.srcObject = stream;
-  await video.play();
+    const canvas = document.getElementById("canvas");
+    canvas.width = SIDE;
+    canvas.height = SIDE;
+    canvas.style.width = '100%';
+    canvas.style.maxWidth = '90vh';
+    canvas.style.margin = '0 auto';
+    canvas.style.display = "block";
+    const context = canvas.getContext("2d");
 
-  const canvas = document.getElementById("canvas");
-  canvas.width = 525;
-  canvas.height = 525;
-  canvas.style.width = "500px";
-  canvas.style.height = "500px";
-  const context = canvas.getContext("2d");
+    function processFrame() {
+      // Draw current video frame to canvas
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-  function processFrame() {
-    // Draw current video frame to canvas
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      // Get pixel data
+      const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+      const pixels = imageData.data;
 
-    // Get pixel data
-    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-    const pixels = imageData.data;
-
-    for (let i = 0; i < pixels.length; i += 4) {
-      if ((i % 35 === 0) || (~~(i / 2_100) % 35 === 0)) {
-        pixels[i] = pixels[i + 1] = pixels[i + 2] = 255;
+      for (let i = 0; i < pixels.length; i += 4) {
+        if ((i % PX_PER_SQUARE === 0) || (~~(i / SIDE_RGBA) % PX_PER_SQUARE === 0)) {
+          pixels[i] = pixels[i + 1] = pixels[i + 2] = 255;
+        }
       }
+
+      context.putImageData(imageData, 0, 0);
     }
 
-    context.putImageData(imageData, 0, 0);
+    setInterval(processFrame, 100);
+  } catch (err) {
+    console.log("ERR", err);
+    debug(String(err));
+    return null;
   }
-
-  setInterval(processFrame, 100);
 }
 
 document.getElementById("start").addEventListener(
