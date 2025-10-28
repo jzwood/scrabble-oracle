21d353224e4d6ad048b0af3ccdd018dd023587b3
Jake Wood <jzwood14@yahoo.com>
2025-10-28 12:31:03 -0400

    wip

--- web/main.js
+++ web/main.js
@@ -4,56 +4,71 @@ function debug(text) {
   body.appendChild(node);
 }
 
-const GRID = 15
-const PX_PER_SQUARE = 35
-const SIDE = PX_PER_SQUARE * GRID
-const SIDE_RGBA = 4 * SIDE
+const GRID = 15;
+const PX_PER_SQUARE = 35;
+const SIDE = PX_PER_SQUARE * GRID;
+const SIDE_RGBA = 4 * SIDE;
 
-async function setupPixelManipulation() {
-  // Get camera access
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: { facingMode: "environment" },
+function getVideo() {
+  return navigator.mediaDevices.getUserMedia({
+    video: { facingMode: "environment" },
+  })
+    .then((stream) => {
+      const video = document.createElement("video");
+      video.srcObject = stream;
+      return video.play().then(() => video);
+    })
+    .then((video) => {
+      const canvas = document.getElementById("canvas");
+      canvas.width = SIDE;
+      canvas.height = SIDE;
+      const context = canvas.getContext("2d");
+      return { video, context };
     });
-    const video = document.createElement("video");
-    video.srcObject = stream;
-    await video.play();
+}
 
-    const canvas = document.getElementById("canvas");
-    canvas.width = SIDE;
-    canvas.height = SIDE;
-    canvas.style.width = '100%';
-    canvas.style.maxWidth = '90vh';
-    canvas.style.margin = '0 auto';
-    canvas.style.display = "block";
-    const context = canvas.getContext("2d");
-
-    function processFrame() {
-      // Draw current video frame to canvas
-      context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-      // Get pixel data
-      const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-      const pixels = imageData.data;
-
-      for (let i = 0; i < pixels.length; i += 4) {
-        if ((i % PX_PER_SQUARE === 0) || (~~(i / SIDE_RGBA) % PX_PER_SQUARE === 0)) {
-          pixels[i] = pixels[i + 1] = pixels[i + 2] = 255;
-        }
-      }
-
-      context.putImageData(imageData, 0, 0);
+function processFrame({ video, context }) {
+  context.drawImage(video, 0, 0, SIDE, SIDE);
+  const imageData = context.getImageData(0, 0, SIDE, SIDE);
+  const pixels = imageData.data;
+  for (let i = 0; i < pixels.length; i += 4) {
+    if (
+      (i % PX_PER_SQUARE === 0) ||
+      (Math.floor(i / SIDE_RGBA) % PX_PER_SQUARE === 0)
+    ) {
+      pixels[i] = pixels[i + 1] = pixels[i + 2] = 255;
     }
+  }
+  context.putImageData(imageData, 0, 0);
+}
 
-    setInterval(processFrame, 100);
-  } catch (err) {
-    console.log("ERR", err);
-    debug(String(err));
-    return null;
+function ready(fn) {
+  if (document.readyState !== "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
   }
 }
 
-document.getElementById("start").addEventListener(
-  "click",
-  setupPixelManipulation,
-);
+function main() {
+  let id = null;
+  let loading = false;
+  document.getElementById("start")
+    .addEventListener("click", () => {
+      if (loading) return null;
+      loading = true;
+      getVideo()
+        .then(({ video, context }) => {
+          clearInterval(id);
+          id = setInterval(() => processFrame({ video, context }), 100);
+        })
+        .catch((err) => {
+          console.error(err);
+        })
+        .finally(() => {
+          loading = false;
+        });
+    });
+}
+
+ready(main);
