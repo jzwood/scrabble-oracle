054d700a15da38d3a418ae3d3d7bed6909c75121
Jake Wood <jzwood14@yahoo.com>
2025-10-31 21:44:57 -0400

    rearranges

--- format.sh
+++ format.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 gleam format src
-deno fmt web/index.html
-deno fmt web/main.js
-deno fmt web/styles.css
-deno fmt web/theme.css
+deno fmt index.html
+deno fmt assets/index.js
+deno fmt assets/css/styles.css
+deno fmt assets/css/theme.css
